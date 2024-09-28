c17204faf2f6b812556ae180682c2fc9f05ea424
Havlight <a24961782@gmail.com>
2024-09-28 18:31:05 +0800

    1

--- query_data.py
+++ query_data.py
@@ -36,13 +36,13 @@ def main():
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=6)
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
 
     # if len(results) == 0 or results[0][1] < 0.1:
     #     print(f"Unable to find matching results.")
     #     return
 
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    context_text = "\n\n---\n\n".join([doc.page_content + "\n" + doc.metadata['source'] for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
     print(prompt)
@@ -50,9 +50,8 @@ def main():
     model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
     response_text = model.invoke(prompt)
 
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
-    print()
+    # sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"\nResponse: {response_text.content}"
     print(formatted_response)
 
 
